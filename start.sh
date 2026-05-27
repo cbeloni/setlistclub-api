d8d9589483f276a704ec5b652fcb7ff0ad3cf87c
Cauê Beloni <cbeloni@gmail.com>
2026-05-26 21:13:32 -0300

    fix venv start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,10 @@
+#!/usr/bin/env bash
+set -e
+
+if [ -f ".venv/bin/activate" ]; then
+  . ".venv/bin/activate"
+elif [ -f "venv/bin/activate" ]; then
+  . "venv/bin/activate"
+fi
+
 python -m uvicorn app.main:app --reload
