c12a60a3fa296b265ecb962f524a301128d99b3e
Cauê Beloni <cbeloni@gmail.com>
2026-05-27 22:32:17 -0300

    cors

--- app/main.py
+++ app/main.py
@@ -27,9 +27,12 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",
+        "http://localhost:5173",
         "http://127.0.0.1:3000",
+        "http://127.0.0.1:5173",
         "https://setlistclub.beloni.dev.br",
     ],
+    allow_origin_regex=r"^https?://(localhost|127\.0\.0\.1)(:\d+)?$",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
